9780d18d568abf1c21695378b5693db8fc9cf472
Mohamed Hesham <mohamed.hesham.2049@gmail.com>
2023-09-28 17:48:33 +0300

    refactor: :recycle: remove Layout component from SideNav

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -9,38 +9,30 @@ const { Sider } = Layout;
 
 const SideNav = () => {
   return (
-    <Layout>
-      <Sider
-        breakpoint='lg'
-        collapsedWidth='0'
-        onBreakpoint={(broken) => {
-          console.log(broken);
-        }}
-        onCollapse={(collapsed, type) => {
-          console.log(collapsed, type);
-        }}
-        style={{
-          background: '#ffffff',
-          padding: 24,
-          boxShadow: '0px 4px 23px 0px rgba(0, 0, 0, 0.05)',
-          height: '100vh',
-          position: 'sticky',
-          zIndex: 100,
-        }}
-      >
-        <Menu mode='inline' style={{ border: 'none' }}>
-          <Menu.Item key='1'>
-            <MenuOutlined style={{ fontSize: 22 }} />
-          </Menu.Item>
-          <Menu.Item key='2' style={{ marginTop: 95 }}>
-            <HomeOutlined style={{ fontSize: 30 }} />
-          </Menu.Item>
-          <Menu.Item key='3'>
-            <ScheduleOutlined style={{ fontSize: 30 }} />
-          </Menu.Item>
-        </Menu>
-      </Sider>
-    </Layout>
+    <Sider
+      style={{
+        background: '#ffffff',
+        boxShadow: '0px 4px 23px 0px rgba(0, 0, 0, 0.05)',
+        position: 'sticky',
+        top: 0,
+        zIndex: 100,
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+      }}
+    >
+      <Menu mode='inline' style={{ border: 'none', marginTop: '50px ' }}>
+        <Menu.Item key='1'>
+          <MenuOutlined style={{ fontSize: 22 }} />
+        </Menu.Item>
+        <Menu.Item key='2' style={{ marginTop: 95 }}>
+          <HomeOutlined style={{ fontSize: 30 }} />
+        </Menu.Item>
+        <Menu.Item key='3' style={{ marginTop: 45 }}>
+          <ScheduleOutlined style={{ fontSize: 30 }} />
+        </Menu.Item>
+      </Menu>
+    </Sider>
   );
 };
 
