a8f5fefc2a9b02ea32469dfddef6d5972914ca71
Mohamed Hesham <mohamed.hesham.2049@gmail.com>
2023-09-28 04:07:23 +0300

    feat: :sparkles: add ParentForm component

---
+++ src/components/ParentForm.tsx
@@ -0,0 +1,41 @@
+import { Button, Form } from 'antd';
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+const ParentForm = () => {
+  const [form] = Form.useForm();
+  const [formValues, setFormValues] = useState({});
+  const formId = uuidv4();
+
+  const onFinish = (values) => {
+    setFormValues({
+      data: {
+        id: formId,
+        type: 'applicationForm',
+        attributes: {
+          ...values,
+        },
+      },
+    });
+  };
+  console.log('formValues ', formValues);
+
+  return (
+    <div className='container' style={{ marginLeft: 170, marginTop: 115 }}>
+      <Form
+        form={form}
+        name='application-form'
+        onFinish={onFinish}
+        style={{ maxWidth: 517 }}
+        colon={false}
+      >
+        <Form.Item>
+          <Button type='primary' htmlType='submit'>
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+export default ParentForm;
