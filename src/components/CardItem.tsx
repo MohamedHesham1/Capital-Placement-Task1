3beec1fb589b8e945eaf3ffefbfd58e2656ec8de
Mohamed Hesham <mohamed.hesham.2049@gmail.com>
2023-09-28 04:04:43 +0300

    feat: :sparkles: update CardItem component with additional props

--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -1,30 +1,43 @@
-import { Card } from 'antd';
+import { Card, Form } from 'antd';
 import { ReactNode } from 'react';
 
 type CardItemProps = {
   children: ReactNode;
+  title: string;
+  padding?: string | number;
+  marginBottom?: string | number;
+  formItemName?: string;
 };
 
-const CardItem = ({ children }: CardItemProps) => (
-  <Card
-    title='Upload cover image'
-    type='inner'
-    bodyStyle={{ padding: 0 }}
-    style={{
-      width: 595,
-      height: 405,
-      borderRadius: 20,
-      boxShadow: '3px 3px 14px 0px rgba(190, 190, 190, 0.30)',
-      overflow: 'hidden',
-    }}
-    headStyle={{
-      background: '#D0F7FA',
-      fontSize: '30px',
-      fontWeight: 'bold',
-    }}
-  >
-    {children}
-  </Card>
+const CardItem = ({
+  children,
+  title,
+  padding = 0,
+  marginBottom = 0,
+  formItemName,
+}: CardItemProps) => (
+  <Form.Item name={formItemName}>
+    <Card
+      title={title}
+      type='inner'
+      bodyStyle={{ padding }}
+      style={{
+        width: 595,
+        borderRadius: 20,
+        boxShadow: '3px 3px 14px 0px rgba(190, 190, 190, 0.30)',
+        overflow: 'hidden',
+        marginBottom: marginBottom,
+      }}
+      headStyle={{
+        background: '#D0F7FA',
+        fontSize: 25,
+        fontWeight: 'bold',
+        height: 93,
+      }}
+    >
+      {children}
+    </Card>
+  </Form.Item>
 );
 
 export default CardItem;
